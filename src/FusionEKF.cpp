0bc76d1f231db1b322dce029d573cf42c96eb65d
Solvejg <asd@nilu.no>
2018-02-19 15:10:27 +0100

    debugging

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -79,6 +79,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    *  Initialization
    ****************************************************************************/
   if (!is_initialized_) {
+	  cout << noise_ax << endl;
     // first measurement
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
@@ -148,13 +149,13 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    *  Update
    ****************************************************************************/
   cout << "x_ predicted = " << ekf_.x_ << endl;
-  cout << "x_ predicted 2 = " << ekf_.x_ << endl;
   cout << "ground truth = " << measurement_pack.raw_measurements_ << endl;
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
 	  //cout << "skip radar" << endl;
 	  if (radar_active) {
+		  cout << "radar" << endl;
 		  Hj_ = tools.CalculateJacobian(ekf_.x_);
 		  ekf_.H_ = Hj_;
 		  ekf_.R_ = R_radar_;
@@ -167,6 +168,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 	  // Laser updates
 	  //cout << "skip Laser" << endl;
 	  if (laser_active) {
+		  cout << "lasar" << endl;
 		  ekf_.H_ = H_laser_;
 		  ekf_.R_ = R_laser_;
 		  ekf_.Update(measurement_pack.raw_measurements_);
