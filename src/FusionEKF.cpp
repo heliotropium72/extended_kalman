2f325edf22f8c3122f5d926b2946b64a3f22a278
Solvejg <asd@nilu.no>
2018-02-19 13:55:07 +0100

    test5

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -38,8 +38,8 @@ FusionEKF::FusionEKF() {
   // measurement matrix Radar: calculate for every step the jacobian Hj_
 
   // acceleration noise
-  float noise_ax = 6;//9;
-  float noise_ay = 6;// 9;
+  float noise_ax = 9;
+  float noise_ay = 9;
 
 }
 
@@ -140,15 +140,16 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    ****************************************************************************/
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
-	  cout << "skip radar" << endl;
-	  //ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
-	  //ekf_.R_ = R_radar_;
-	  //ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+	  //cout << "skip radar" << endl;
+	  ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+	  ekf_.R_ = R_radar_;
+	  ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
-	  ekf_.H_ = H_laser_;
-	  ekf_.R_ = R_laser_;
-	  ekf_.Update(measurement_pack.raw_measurements_);
+	cout << "skip Laser" << endl;
+	  //ekf_.H_ = H_laser_;
+	  //ekf_.R_ = R_laser_;
+	  //ekf_.Update(measurement_pack.raw_measurements_);
 	
   }
 
